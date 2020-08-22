763dae0da5d7dcaf98010080f2379ceb21ec6e54
DiptaKumarNath <54820854+DiptaKumarNath@users.noreply.github.com>
2020-08-22 21:56:19 +0600

    Initial commit

---
+++ lib/dummy_data.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/model/category.dart';
+const Dummy_categories = const [
+  Category(
+    title: 'Italian',
+    id: 'c1',
+    color: Colors.purple
+  ),
+  Category(
+      title: 'Quick & Easy',
+      id: 'c2',
+      color: Colors.orange
+  ),
+  Category(
+      title: 'Bangladeshi',
+      id: 'c3',
+      color: Colors.green
+  ),
+  Category(
+      title: 'Indian',
+      id: 'c4',
+      color: Colors.purple
+  ),
+];
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Desi Meal",
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith()
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Desi Meal"),
+      ),
+      body: Center(
+        child: Text("This is the center"),
+      ),
+    );
+  }
+}
+
+
